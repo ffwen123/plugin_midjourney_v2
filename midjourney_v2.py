5381d529abd1454fa4e4076dfbe8fcf076c9a9aa
ffwen123 <ffwen123@163.com>
2023-05-31 17:19:37 +0800

    img2img

--- midjourney_v2.py
+++ midjourney_v2.py
@@ -245,10 +245,13 @@ class MidjourneyV2(Plugin):
             if api_data.status_code == 200:
                 if api_data.json().get("code", 0) == 2:
                     time.sleep(20)
-                time.sleep(5)
+                time.sleep(10)
                 get_resp = requests.get(url=self.call_back_url.format(messageId), timeout=120.05)
                 if not get_resp.text:
-                    return "已失效", None
+                    time.sleep(20)
+                    get_resp = requests.get(url=self.call_back_url.format(messageId), timeout=120.05)
+                    if not get_resp.text:
+                        return "已失效", None
                 out_time = time.time()
                 logger.info("[RP] get_resp={}".format(get_resp.text))
                 # Webhook URL的响应慢，没隔 5 秒获取一次，超过600秒判断没有结果
